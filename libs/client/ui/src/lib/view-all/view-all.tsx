3513149d274aef22a73fcff0a7e9d0a2b8ae7032
u20437863-James-Butler <u20437863@tuks.co.za>
2022-05-11 21:14:19 +0200

    Initial ViewAll files

---
+++ libs/client/ui/src/lib/view-all/view-all.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+/* eslint-disable-next-line */
+export interface ViewAllProps {}
+
+export function ViewAll(props: ViewAllProps) {
+  return (
+    <View>
+      <Text>Welcome to ViewAll!</Text>
+    </View>
+  );
+}
+
+export default ViewAll;
