ed3f3fd2d4d74cd9b3750c99f8b3f30d05b1e864
u20437863-James-Butler <u20437863@tuks.co.za>
2022-05-12 00:12:42 +0200

    Added more styles

--- libs/client/ui/src/lib/view-all/view-all.js
+++ libs/client/ui/src/lib/view-all/view-all.js
@@ -142,4 +142,206 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
     justifyContent: 'flex-start'
   },
+  viewAllBottomBar: {
+    width: '100%',
+    flexGrow: 1,
+    backgroundColor: '#c4c4c4ff',
+    shadowColor:
+      'transparent' /* cannot find mapping from CSS: 0px -4px 4px 0px rgba(0,0,0,0.09803921568627451), https://ethercreative.github.io/react-native-shadow-generator/ */
+  },
+  orderByLabel: {
+    color: '#344053ff',
+    textAlign: 'center',
+    letterSpacing: 0,
+    lineHeight: 20,
+    fontSize: 14,
+    fontWeight: '500',
+    fontStyle: 'normal',
+    fontFamily: 'System' /* Inter */,
+    paddingHorizontal: 0,
+    paddingVertical: 0
+  },
+  orderByLabel_box: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  },
+  bottomFlex: {
+    flexGrow: 1,
+    flexDirection: 'row'
+  },
+  bottomFlex_item: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 42
+  },
+  backIcon: {
+    resizeMode: 'contain'
+  },
+  bottomFlex_space: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 76
+  },
+  bottomFlex_item1: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 24
+  },
+  moreOptionsIcon: {
+    resizeMode: 'contain'
+  },
+  bottomFlex_space1: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 67
+  },
+  bottomFlex_item2: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 153
+  },
+  block12: {
+    width: '100%',
+    flexGrow: 1
+  },
+  block13: {
+    width: '100%',
+    flexGrow: 1
+  },
+  group: {
+    width: '100%',
+    flexGrow: 1
+  },
+  pdfFrame: {},
+  pdfFrame_layout: {
+    position: 'absolute',
+    top: -628,
+    height: 1290,
+    left: -206,
+    width: 356
+  },
+  pdfFrame_item: {
+    flexGrow: 0,
+    flexShrink: 1
+  },
+  pdfTile: {
+    flexGrow: 1,
+    borderRadius: 5,
+    borderStyle: 'solid',
+    borderColor: '#d0d5ddff',
+    borderWidth: 1,
+    elevation: 2,
+    shadowColor: '#000000',
+    shadowRadius: 2.621621621621622,
+    shadowOpacity: 0.2173913043478261,
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    flexDirection: 'row'
+  },
+  pdfTile_item: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 90
+  },
+  pdfThumbnail: {
+    resizeMode: 'contain',
+    borderRadius: 5
+  },
+  pdfThumbnail_layout: {
+    marginTop: 0,
+    height: 127,
+    marginBottom: 0,
+    marginLeft: 0,
+    width: 90,
+    minWidth: 90,
+    marginRight: 0
+  },
+  pdfTile_space: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 13
+  },
+  pdfTile_item1: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 253
+  },
+  pdfTileInfo: {
+    flexGrow: 1,
+    flexDirection: 'row'
+  },
+  pdfTileInfo_item: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 220
+  },
+  pdfTileText: {
+    flexGrow: 1
+  },
+  pdfTileText_item: {
+    flexGrow: 0,
+    flexShrink: 1
+  },
+  pdfName: {
+    color: '#344053ff',
+    textAlign: 'left',
+    letterSpacing: 0,
+    lineHeight: 20,
+    fontSize: 16,
+    fontWeight: '400',
+    fontStyle: 'normal',
+    fontFamily: 'System' /* Inter */,
+    paddingHorizontal: 0,
+    paddingVertical: 0
+  },
+  pdfName_box: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  pdfDate: {
+    color: '#667084ff',
+    textAlign: 'left',
+    letterSpacing: 0,
+    lineHeight: 20,
+    fontSize: 14,
+    fontWeight: '400',
+    fontStyle: 'normal',
+    fontFamily: 'System' /* Inter */,
+    paddingHorizontal: 0,
+    paddingVertical: 0
+  },
+  pdfDate_box: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start'
+  },
+  pdfTileInfo_item1: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 33
+  },
+  downloadState: {
+    width: '100%',
+    flexGrow: 1
+  },
+  downloadIcon: {
+    resizeMode: 'contain'
+  },
+  downloadIcon_layout: {
+    marginTop: 10,
+    height: 18,
+    marginBottom: 10,
+    marginLeft: 4,
+    width: 18,
+    minWidth: 18,
+    marginRight: 11
+  },
+  
 });
