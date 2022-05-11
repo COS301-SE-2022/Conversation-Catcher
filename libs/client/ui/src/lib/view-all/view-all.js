7284bdf9189157c4e937a16c1aa73b285eb9c1cc
u20437863-James-Butler <u20437863@tuks.co.za>
2022-05-11 23:56:02 +0200

    Added js files

---
+++ libs/client/ui/src/lib/view-all/view-all.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export function ViewAll(props) {
+  return (
+    <View>
+      <Text>Welcome to ViewAll!</Text>
+    </View>
+  );
+}
+export default ViewAll;
