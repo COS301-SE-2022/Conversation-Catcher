d86e8f79d446483abffc60a30ad81a5268a5679a
RRduToit <u20450533@Tuks.co.za>
2022-05-11 14:36:17 +0200

    :sparkles:  Adding to API

---
+++ libs/api/PDF-manager/api/feature/src/lib/pdf-manager.resolver.ts
@@ -0,0 +1,6 @@
+//import { Resolver, Query, Args } from '@nestjs/graphql';
+
+//@Resolver((of) => PdfManagerEntity)
+export class PdfManagerResolver {
+    //constructor(private companyService: CompanyExploreService) {}
+}
---
+++ libs/api/PDF-manager/repository/src/lib/pdf-manager-repository.spec.ts
@@ -0,0 +1,7 @@
+import { PdfManagerRepository } from './pdf-manager-repository';
+
+describe('PdfManagerRepository', () => {
+  it('should be defined', () => {
+    expect(new PdfManagerRepository()).toBeDefined();
+  });
+});
