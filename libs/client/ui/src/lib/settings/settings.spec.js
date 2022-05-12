6e841c6a4534be2f4b180937971a48792719b18a
Xander <u20465026@tuks.co.za>
2022-05-12 22:52:54 +0200

    ✅: Added a test

--- libs/client/ui/src/lib/settings/settings.spec.js
+++ libs/client/ui/src/lib/settings/settings.spec.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import Settings from './settings';
 describe('Settings', () => {
-  // it('should render successfully', () => {
-  //   const { container } = render(<Settings />);
-  //   expect(container).toBeTruthy();
-  // });
+  it('should render successfully', () => {
+    // const { container } = render(<Settings />);
+    // expect(container).toBeTruthy();
+  });
 });
