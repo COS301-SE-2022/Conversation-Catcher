c237701dd7458d46b1ded376ad83fc4f17e8ab04
Xander <u20465026@tuks.co.za>
2022-05-13 03:00:04 +0200

    ✅: Add unit tests that use snapshots

--- libs/client/ui/src/lib/settings/settings.spec.js
+++ libs/client/ui/src/lib/settings/settings.spec.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import Settings from './settings';
-describe('Settings', () => {
+import { create } from 'react-test-renderer';
+describe('Setup of settings', () => {
   it('should render successfully', () => {
-    // const { container } = render(<Settings />);
-    // expect(container).toBeTruthy();
+    const { container } = render(<Settings />);
+    expect(container).toBeTruthy();
+  });
+
+  const tree = create(<Settings />);
+  test('snapshot', () => {
+    expect(tree).toMatchSnapshot();
   });
 });
+
+// describe('The actions that occur on button press', () => {
+//   it('should display the available colours')
+// });
