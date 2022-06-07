ca8cfadc52bb9e001a8b929bc989a16e822d9b92
Xander <u20465026@tuks.co.za>
2022-06-07 16:16:04 +0200

    ✨:Connect to mongodb database

--- libs/api/database-manager/api/feature/src/lib/api-database-manager-api-feature.module.ts
+++ libs/api/database-manager/api/feature/src/lib/api-database-manager-api-feature.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseManagerResolver } from './database-manager.resolver';
+import { ApiDatabaseManagerServiceFeatureModule } from '@conversation-catcher/api/database-manager/service/feature'
 
 @Module({
   controllers: [],
   providers: [DatabaseManagerResolver],
   exports: [DatabaseManagerResolver],
-  imports: [],
+  imports: [ApiDatabaseManagerServiceFeatureModule],
 })
 export class ApiDatabaseManagerApiFeatureModule {}
--- libs/api/database-manager/api/feature/src/lib/database-manager.resolver.ts
+++ libs/api/database-manager/api/feature/src/lib/database-manager.resolver.ts
@@ -1,37 +1,15 @@
 import { Query, Args, Resolver, Mutation } from '@nestjs/graphql';
+import { DatabaseManagerService } from '@conversation-catcher/api/database-manager/service/feature';
+import { tap, map } from 'rxjs';
 
 @Resolver()
 export class DatabaseManagerResolver {
-  @Query(() => String, {name: 'test'})
-  pingStudentProfiles() {
-    
-    const data = JSON.stringify({
-      "collection": "<COLLECTION_NAME>",
-      "database": "<DATABASE_NAME>",
-      "dataSource": "Cluster0",
-      "projection": {
-          "_id": 1
-      }
-  });
+  constructor(private dbService: DatabaseManagerService) {}
 
-  const config = {
-      method: 'post',
-      url: 'https://data.mongodb-api.com/app/data-dtzbr/endpoint/data/beta/action/findOne',
-      headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Request-Headers': '*',
-          'api-key': '<API_KEY>'
-      },
-      data : data
-  };
-
-  axios(config)
-      .then(function (response) {
-          console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-          console.log(error);
-      });
+  @Query(() => String, { name: 'test' })
+  async pingStudentProfiles() {
+    const a = await this.dbService.getResult();
+    console.log(a);
     return 'on';
   }
 }
