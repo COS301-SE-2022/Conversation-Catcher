f33fb739293efb284a35bd38a40a1757a2c519e8
Xander <u20465026@tuks.co.za>
2022-06-07 17:08:25 +0200

    ✅: Add mock for HTTPRequest to pass dependency problem

--- libs/api/database-manager/service/feature/src/lib/database-manager.service.spec.ts
+++ libs/api/database-manager/service/feature/src/lib/database-manager.service.spec.ts
@@ -1,13 +1,16 @@
 import { Test } from '@nestjs/testing';
 import { DatabaseManagerService } from './database-manager.service';
+import { HttpService } from '@nestjs/axios';
 
 describe('ApiDatabaseManagerServiceFeatureService', () => {
   let service: DatabaseManagerService;
 
+  const MockHTTPService = {}
+
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [DatabaseManagerService],
-    }).compile();
+      providers: [DatabaseManagerService,HttpService],
+    }).overrideProvider(HttpService).useValue(MockHTTPService).compile();
 
     service = module.get(DatabaseManagerService);
   });
