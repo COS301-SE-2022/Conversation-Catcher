824c95695153781af427aa18e5981fa77538787c
RRduToit <u20450533@Tuks.co.za>
2022-06-08 18:16:02 +0200

    change

--- libs/api/PDF-manager/api/data-access/src/lib/pdf-manager.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PdfManagerEntity } from './pdf-manager.entity';
-
-describe('PdfManagerEntity', () => {
-  it('should be defined', () => {
-    expect(new PdfManagerEntity()).toBeDefined();
-  });
-});
--- libs/api/PDF-manager/api/feature/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/api-pdf-manager-api-feature.module';
-export * from './lib/pdf-manager.resolver'
