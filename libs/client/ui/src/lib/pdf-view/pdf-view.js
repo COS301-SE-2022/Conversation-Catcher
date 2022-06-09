75e596140359da7c51c20ed8739a510367052961
Klara Meyer <u20428082@tuks.co.za>
2022-06-09 02:49:02 +0200

    pdf view page

--- libs/client/ui/src/lib/pdf-view/pdf-view.js
+++ libs/client/ui/src/lib/pdf-view/pdf-view.js
@@ -1,9 +1,74 @@
-import styles from './pdf-view.module.css';
-export function PdfView(props) {
+import React from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, Alert } from 'react-native';
+import Pdf from 'react-native-pdf';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export const PdfView = ({ navigation }) =>  {
+  const {
+    source
+  } = this.props
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to PdfView!</h1>
-    </div>
+    
+    <View style={styles.pdfPage}>
+      <Pdf 
+        style={styles.pdfView}
+        source={source}
+      />
+      
+      <View style={styles.pdfBar}>
+        <TouchableOpacity
+              style={styles.moreModalButton}
+              onPress={() => Alert.alert('click')}>
+              <View style={styles.iconContainer}>
+                <Icon 
+                  style={styles.moreModalButtonIcon}
+                  name="paper-plane-o"
+                  size={18}
+                />
+              </View>
+            </TouchableOpacity>
+
+            <View style={styles.moreModalButtonDivider} /> 
+
+            <TouchableOpacity
+              style={styles.moreModalButton}
+              onPress={() => Alert.alert('click')}>
+              <View style={styles.iconContainer}>
+                <Icon 
+                  style={styles.moreModalButtonIcon}
+                  name="pencil-square-o"
+                  size={20}
+                />
+              </View>
+            </TouchableOpacity>
+
+            <View style={styles.moreModalButtonDivider} /> 
+
+            <TouchableOpacity 
+              style={styles.moreModalButton}
+              onPress={() => Alert.alert('click')}>
+              <View style={styles.iconContainer}>
+                <Icon 
+                  style={styles.moreModalButtonIcon}
+                  name="trash-o"
+                  size={20}
+                />
+              </View>
+            </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 export default PdfView;
+
+const styles = StyleSheet.create({
+  pdfPage: {
+
+  },
+  pdfView: {
+
+  },
+  pdfBar: {
+
+  },
+})
