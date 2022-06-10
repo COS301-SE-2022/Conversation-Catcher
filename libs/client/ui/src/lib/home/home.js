0162b0a22f08359b92ef462813bb75a4c0130610
Klara Meyer <u20428082@tuks.co.za>
2022-06-10 06:23:32 +0200

    Revert "Update home.js"
    This reverts commit e2d5855c3bed608c876ad53da934fd6516edf90f.

--- libs/client/ui/src/lib/home/home.js
+++ libs/client/ui/src/lib/home/home.js
@@ -7,7 +7,7 @@ import Modal from 'react-native-modal';
 import { Buffer } from 'buffer';
 //import Permissions from 'react-native-permissions';
 //import Sound from 'react-native-sound';
-import AudioRecord from 'react-native-audio-record';
+//import AudioRecord from 'react-native-audio-record';
 import DocumentPicker, { types } from 'react-native-document-picker';
 
 export const Home = ({ navigation }) => {
@@ -107,13 +107,13 @@ export const Home = ({ navigation }) => {
       wavFile: 'test.wav'
     };
 
-    AudioRecord.init(options);
+//    AudioRecord.init(options);
 
-    AudioRecord.on('data', data => {
+{/*    AudioRecord.on('data', data => {
       const chunk = Buffer.from(data, 'base64');
       console.log('chunk size', chunk.byteLength);
       // do something with audio chunk
-    });
+    });*/} 
   }
 
   const checkPermission = async () => {
@@ -141,13 +141,13 @@ export const Home = ({ navigation }) => {
     state.audioFile = '';
     state.recording = true;
     state.loaded = false;
-    AudioRecord.start();
+    //AudioRecord.start();
   };
 
   const stop = async () => {
     if (!state.recording) return;
     console.log('stop record');
-    state.audioFile = await AudioRecord.stop();
+    //state.audioFile = await AudioRecord.stop();
     console.log('audioFile', state.audioFile);
     //componentDidMount();
     state.audioFile = false;
