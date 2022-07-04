ae9b92a19ebcdaa8d3613ca6c7f409949a08e803
Xander <u20465026@tuks.co.za>
2022-07-04 15:24:21 +0200

    🐛: update spec file for mongodb access + fix mutation

--- libs/api/pdf-manager/repository/data-access/src/lib/mongodb-access.spec.ts
+++ libs/api/pdf-manager/repository/data-access/src/lib/mongodb-access.spec.ts
@@ -1,7 +1,8 @@
 import { MongoDBAccess } from './mongodb-access';
+import { HttpService } from '@nestjs/axios';
 
 describe('MongoAccess', () => {
   it('should be defined', () => {
-    expect(new MongoDBAccess()).toBeDefined();
+    expect(new MongoDBAccess(new HttpService)).toBeDefined();
   });
 });
--- libs/api/shell/src/lib/api-shell.module.ts
+++ libs/api/shell/src/lib/api-shell.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApiDatabaseManagerApiFeatureModule as DatabaseManager } from '@conversation-catcher/api/database-manager/api/feature';
-import { ApiStudentExploreApiFeatureModule as PdfManager } from '@conversation-catcher/api/pdf-manager/api/feature'
+import { ApiStudentExploreApiFeatureModule as PdfManager } from '@conversation-catcher/api/pdf-manager/api/feature';
 
 @Module({
   controllers: [],
@@ -14,8 +14,8 @@ import { ApiStudentExploreApiFeatureModule as PdfManager } from '@conversation-c
       driver: ApolloDriver,
       playground: true,
     }),
-    DatabaseManager,
-    // PdfManager
+    // DatabaseManager,
+    PdfManager,
   ],
 })
 export class ApiShellModule {}
