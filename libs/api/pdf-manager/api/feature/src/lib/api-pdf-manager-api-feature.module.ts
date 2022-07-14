da52c18812d41edb0a0cd753da4aedcd9572bc7f
Xander <u20465026@tuks.co.za>
2022-07-15 00:51:42 +0200

    ✅: pass pdf-manager-api-test

--- libs/api/pdf-manager/api/feature/src/lib/api-pdf-manager-api-feature.module.ts
+++ libs/api/pdf-manager/api/feature/src/lib/api-pdf-manager-api-feature.module.ts
@@ -1,27 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ApiPdfManagerApiFeatureResolver } from './api-pdf-manager-api-feature.resolver';
-import { ApiPdfManagerServiceFeatureService, ApiPdfManagerServiceFeatureModule } from '@conversation-catcher/api/pdf-manager/service/feature';
-import { ApiPdfManagerServiceModule } from '@conversation-catcher/api/pdf-manager/service'
-import { CommandBus, CqrsModule, QueryBus } from '@nestjs/cqrs';
-
-/*@Module({
-  imports: [ApiPdfManagerApiFeatureResolver],
-  controllers: [],
-  providers: [ApiPdfManagerServiceFeatureService],
-  exports: [ApiPdfManagerApiFeatureResolver],
-})
-export class ApiPdfManagerApiFeatureModule {}*/
+// import { ApiPdfManagerServiceFeatureService, ApiPdfManagerServiceFeatureModule } from '@conversation-catcher/api/pdf-manager/service/feature';
+import { ApiPdfManagerServiceModule } from '@conversation-catcher/api/pdf-manager/service';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-  imports: [ApiPdfManagerServiceFeatureModule, CqrsModule, ApiPdfManagerServiceModule],
+  imports: [CqrsModule, ApiPdfManagerServiceModule],
   controllers: [],
   providers: [
-    CommandBus,
-    QueryBus,
-    //PrismaService,
     ApiPdfManagerApiFeatureResolver,
-    ApiPdfManagerServiceFeatureService,
-  ]
-
+  ],
 })
 export class ApiStudentExploreApiFeatureModule {}
