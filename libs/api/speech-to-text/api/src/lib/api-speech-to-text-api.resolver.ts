bbcf2adb126a00d54c74a9d3492b22284e80bd7a
RRduToit <u20450533@Tuks.co.za>
2022-07-14 09:22:21 +0200

    Update api-speech-to-text-api.resolver.ts

--- libs/api/speech-to-text/api/src/lib/api-speech-to-text-api.resolver.ts
+++ libs/api/speech-to-text/api/src/lib/api-speech-to-text-api.resolver.ts
@@ -1,7 +1,6 @@
 import { Resolver } from '@nestjs/graphql';
 import { Mutation, Args } from '@nestjs/graphql';
 import { ApiSpeechToTextServiceService } from "@conversation-catcher/api/speech-to-text/service";
-import { PdfEntity } from "@conversation-catcher/api/pdf-manager/api/data-access"
 
 @Resolver()
 export class ApiSpeechToTextApiResolver {
