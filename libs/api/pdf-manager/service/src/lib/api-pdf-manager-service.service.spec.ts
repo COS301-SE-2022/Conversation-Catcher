92d35b23af0cf8c5bc2372b3eb17155700005e43
Xander <u20465026@tuks.co.za>
2022-07-15 00:42:22 +0200

    ✅: Pass service layer unit test

--- libs/api/pdf-manager/service/src/lib/api-pdf-manager-service.service.spec.ts
+++ libs/api/pdf-manager/service/src/lib/api-pdf-manager-service.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { ApiPdfManagerServiceService } from './api-pdf-manager-service.service';
+import { QueryBus, CommandBus } from '@nestjs/cqrs'
 
 describe('ApiPdfManagerServiceService', () => {
   let service: ApiPdfManagerServiceService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [ApiPdfManagerServiceService],
+      providers: [ApiPdfManagerServiceService, QueryBus, CommandBus],
     }).compile();
 
     service = module.get(ApiPdfManagerServiceService);
