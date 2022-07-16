c7f8e82dd195d2d25eff83473948426b1fac5711
Klara Meyer <u20428082@tuks.co.za>
2022-07-16 17:39:30 +0200

    Update summarise-text-model.py

--- libs/api/summarise-text/AI/summarise-text-model.py
+++ libs/api/summarise-text/AI/summarise-text-model.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from nltk.corpus import stopwords
-from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Concatenate, TimeDistributed
-from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.python.keras.layers import Input, LSTM, Embedding, Dense, Concatenate, TimeDistributed
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.callbacks import EarlyStopping
 import warnings
 
 pd.set_option("display.max_colwidth", 200)
