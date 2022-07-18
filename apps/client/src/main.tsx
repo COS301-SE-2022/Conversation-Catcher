93b1712485b9951eb7961f5bc9943aa88c7a2695
u20437863-James-Butler <u20437863@tuks.co.za>
2022-07-18 20:53:30 +0200

    Add provider to Main

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,4 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
 import { AppRegistry } from 'react-native';
 import App from './app/App';
+import { Provider } from 'react-redux';
 
-AppRegistry.registerComponent('Client', () => App);
+const store = configureStore()
+
+const RNRedux = () => (
+    <Provider store = { store }>
+        <App/>
+    </Provider>
+)
+
+AppRegistry.registerComponent('Client', () => RNRedux);
