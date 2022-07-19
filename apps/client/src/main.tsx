62920f0c6a4e814a1dd1a28b0e9abd9b2519a686
u20437863-James-Butler <u20437863@tuks.co.za>
2022-07-19 23:24:10 +0200

    Added configureStore params

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,8 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import { AppRegistry } from 'react-native';
 import App from './app/App';
 import { Provider } from 'react-redux';
+import { colourReducer } from './app/slices/colour.slice';
 
-const store = configureStore()
+const store = configureStore({reducer: colourReducer})
 
 const RNRedux = () => (
     <Provider store = { store }>
