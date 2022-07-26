e16184e5a52e1e6348b7cdf1714e49306459a164
Klara Meyer <u20428082@tuks.co.za>
2022-07-26 17:59:31 +0200

    Update pdf-view.js

--- libs/client/ui/src/lib/shared-components/pdf-view/pdf-view.js
+++ libs/client/ui/src/lib/shared-components/pdf-view/pdf-view.js
@@ -1,128 +1,375 @@
-import React from 'react';
-import { View, StyleSheet, Text, ImageBackground, TouchableOpacity, Alert} from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+  TextInput,
+} from 'react-native';
+import { gql, useQuery, useMutation } from '@apollo/client';
+import PdfTile from '../shared-components/pdf-tile/pdf-tile.js';
+import ModalDropdown from 'react-native-modal-dropdown';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Modal from 'react-native-modal';
+import Share from 'react-native-share';
 import colour from '../colour/colour';
 
-
 export const PdfView = ({ navigation }) => {
-  return (
-    <View style={styles.settings}>
-      < View style={styles.big_title_box}>
-        <Text style={styles.big_title}>
-          {'Settings'}
-        </Text>
-      </ View>
-      < View style={styles.settingsBody}>
+  const [moreVisible, setMoreVisible] = useState(false);
+  const [selectMode, setSelectMode] = useState(false);
+  const [bottomModalVisible, setBottomModalVisible] = useState(false);
+  const [bottomModalType, setBottomModalType] = useState('none');
+  const [renameModalVisible, setRenameModalVisible] = useState(false);
+
+  const url = 'https://awesome.contents.com/';
+  const title = 'Awesome Contents';
+  const message = 'Please check this out.';
+
+  const options = {
+    title,
+    url,
+    message,
+  };
+
+  const share = async (customOptions = options) => {
+    try {
+      await Share.open(customOptions);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  function BottomModalButton(props) {
+    if (props.type === 'share') {
+      return (
         <TouchableOpacity
-          style={styles.settingsButton}
-          onPress={() => navigation.navigate('ChangeEmail')}>
-          <View style={styles.settingsButtonContent}>
-            <View style={styles.iconContainer}>
-              <Icon 
-                style={[styles.settingsButtonIcon, {color : colour.state}]}
-                name="envelope"
-                size={18}
-              />
-            </View>
-            <View style={styles.settingsButtonText_box}>
-              <Text style={styles.settingsButtonText} ellipsizeMode={'clip'}>
-                {'Change email'}
-              </Text>
-            </View>
-          </View>   
+          style={styles.backButton}
+          onPress={async () => {
+            await share({
+              title: 'Sharing pdf file from awesome share app',
+              message: 'Please take a look at this file',
+              url: '../assets/thereactnativebook-sample.pdf',
+            });
+            setSelectMode(false);
+            setBottomModalVisible(false);
+          }}
+        >
+          <Icon name="paper-plane-o" color="#ffffffff" size={22} />
         </TouchableOpacity>
-
-        <View style={styles.settingsButtonDivider} /> 
-
+      );
+    }
+    if (props.type === 'rename') {
+      return (
         <TouchableOpacity
-          style={styles.settingsButton}
-          onPress={() => navigation.navigate('ChangePassword')}>
-          <View style={styles.settingsButtonContent}>
-            <View style={styles.iconContainer}>
-              <Icon 
-                style={{color : colour.state}}
-                name="lock"
-                size={20}
-              />
-            </View>
-            <View style={styles.settingsButtonText_box}>
-              <Text style={styles.settingsButtonText}>
-                {'Change password'}
-              </Text>
-            </View>
-          </View>
+          style={styles.backButton}
+          onPress={() => setBottomModalVisible(false)}
+        >
+          <Icon name="pencil-square-o" color="#ffffffff" size={22} />
         </TouchableOpacity>
-
-        <View style={styles.settingsButtonDivider} /> 
-
-        <TouchableOpacity 
-          style={styles.settingsButton}
-          onPress={() => navigation.navigate('Colour')}>
-          <View style={styles.settingsButtonContent}>
-            <View style={styles.iconContainer}>
-              <Icon 
-                style={{color : colour.state}}
-                name="sliders"
-                size={20}
-              />
-            </View>
-            <View style={styles.settingsButtonText_box}>
-              <Text style={styles.settingsButtonText}>
-                {'Change colour'}
-              </Text>
-            </View>
-          </View> 
-        </TouchableOpacity>
-
-        <View style={styles.settingsButtonDivider} /> 
-
-        <TouchableOpacity
-          style={styles.settingsButton}
-          onPress={() => navigation.navigate('Login')}>
-          <View style={styles.settingsButtonContent}>
-            <View style={styles.iconContainer}>
-              <Icon 
-                style={[styles.settingsButtonIcon, {color : colour.state}]}
-                name="sign-out"
-                size={20}
-              />
-            </View>
-            <View style={styles.settingsButtonText_box}>
-              <Text style={styles.settingsButtonText}>
-                {'Log out'}
-              </Text>
-            </View>
-          </View>
-        </TouchableOpacity>
-      </View> 
-
-      <TouchableOpacity  
-        style={styles.backButton} 
-        onPress={() => navigation.navigate('Home')}>
-          <Icon 
-            name="angle-left"
-            color={colour.state}
-            size={28}
-          />
+      );
+    }
+    return (
+      <TouchableOpacity
+        style={styles.backButton}
+        onPress={() => {
+          setSelectMode(false);
+          setBottomModalVisible(false);
+        }}
+      >
+        <Icon name="trash-o" color="#ffffffff" size={22} />
       </TouchableOpacity>
-    </ View>
-  );
-}
+    );
+  }
 
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message:
+          'React Native | A framework for building native apps using React',
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const GET_USER_PDFS = gql`
+    query getForUser {
+      getPDFs(id: "John@test") {
+        id
+        name
+        # creationDate,
+        downloaded
+        #pdf
+      }
+    }
+  `;
+
+  // const INCREMENT_COUNTER = gql`
+  //   mutation myMutation {
+  //     downloadedPDF(id: "id") {
+  //       id
+  //       name
+  //       downloaded
+  //     }
+  //   }
+  // `;
+
+  // function useSetName() {
+  //   console.log('setNameCalled');
+  //   const [mutateFunction, { data, loading, error }] =
+  //     useMutation(INCREMENT_COUNTER);
+  //   mutateFunction();
+  //   console.log(data);
+  //   return <p>This is me!</p>;
+  // }
+
+  // function useGetPdfs() {
+  //   const { data, loading, error } = useQuery(GET_USER_PDFS);
+  //   console.log("GetPdfs");
+  //   console.log(data);
+  //   console.log(loading);
+  //   console.log(error);
+  //   if (loading)
+  //   return (
+  //     <ScrollView style={styles.recentPdfTiles}>
+  //       <Text>loading...</Text>
+  //     </ScrollView>
+  //   )
+
+  //   if (error)
+  //   return (
+  //     <ScrollView style={styles.recentPdfTiles}>
+  //       <Text>An error occured...</Text>
+  //       <Text>error[0]</Text>
+  //     </ScrollView>
+  //   )
+
+  //   return (<ScrollView style={styles.recentPdfTiles}>
+  //     { data.getPDFs.map((item,key)=>(
+  //       <PdfTile
+  //       id= {key + 1}
+  //       name= {item.name}
+  //       date="13 Apr 2022, 11:53"
+  //       source={''}
+  //       text={item.pdf}
+  //       downloaded={item.downloaded}
+  //       showCheck={selectMode}
+  //       pdfSource=""
+  //       nav={navigation}
+  //     />)
+  //     ) }
+
+  //   </ScrollView> );
+  // }
+
+  return (
+    <View style={styles.viewAllPage}>
+      <View style={styles.viewAllTopBar}>
+        <View style={styles.big_title_box}>
+          <Text style={styles.big_title}>{'PDFs'}</Text>
+        </View>
+
+        
+      </View>
+
+      <Text>
+
+      </Text>
+
+      <View style={styles.viewAllBottomBar}>
+        <TouchableOpacity
+          style={styles.moreButton}
+          onPress={() => setMoreVisible(true)}
+        >
+          <Icon name="ellipsis-h" color="#344053ff" size={30} />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.navigate('Home')}
+        >
+          <Icon name="angle-left" color="#344053ff" size={30} />
+        </TouchableOpacity>
+
+      </View>
+
+      <Modal
+        style={styles.modal}
+        isVisible={moreVisible}
+        avoidKeyboard={true}
+        hasBackdrop={true}
+        backdropColor="white"
+        onBackdropPress={() => setMoreVisible(false)}
+      >
+        <View style={styles.moreModalInner}>
+          <TouchableOpacity
+            style={styles.moreModalButton}
+            onPress={() => {
+              setBottomModalType('share');
+              setSelectMode(true);
+              setBottomModalVisible(true);
+              setMoreVisible(false);
+            }}
+          >
+            <View style={styles.moreModalButtonContent}>
+              <View style={styles.iconContainer}>
+                <Icon
+                  style={{ color: colour.state }}
+                  name="paper-plane-o"
+                  size={18}
+                />
+              </View>
+              <View style={styles.moreModalButtonText_box}>
+                <Text style={styles.moreModalButtonText} ellipsizeMode={'clip'}>
+                  {'Share'}
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+
+          <View style={styles.moreModalButtonDivider} />
+
+          <TouchableOpacity
+            style={styles.moreModalButton}
+            onPress={() => {
+              setBottomModalType('rename');
+              setBottomModalVisible(true);
+              setMoreVisible(false);
+            }}
+          >
+            <View style={styles.moreModalButtonContent}>
+              <View style={styles.iconContainer}>
+                <Icon
+                  style={{ color: colour.state }}
+                  name="pencil-square-o"
+                  size={20}
+                />
+              </View>
+              <View style={styles.moreModalButtonText_box}>
+                <Text style={styles.moreModalButtonText}>{'Rename'}</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+
+          <View style={styles.moreModalButtonDivider} />
+
+          <TouchableOpacity
+            style={styles.moreModalButton}
+            onPress={() => {
+              setBottomModalType('delete');
+              setBottomModalVisible(true);
+              setSelectMode(true);
+              setMoreVisible(false);
+            }}
+          >
+            <View style={styles.moreModalButtonContent}>
+              <View style={styles.iconContainer}>
+                <Icon
+                  style={{ color: colour.state }}
+                  name="trash-o"
+                  size={20}
+                />
+              </View>
+              <View style={styles.moreModalButtonText_box}>
+                <Text style={styles.moreModalButtonText}>{'Delete'}</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+
+      <Modal
+        isVisible={bottomModalVisible}
+        coverScreen={false}
+        hasBackdrop={false}
+        style={{
+          width: '100%',
+          height: '8%',
+          margin: 0,
+          justifyContent: 'flex-end',
+        }}
+      >
+        <View
+          style={[styles.modalBottomBar, { backgroundColor: colour.state }]}
+        >
+          <TouchableOpacity
+            style={styles.backButton}
+            onPress={() => {
+              setBottomModalVisible(false);
+              setSelectMode(false);
+            }}
+          >
+            <Icon name="angle-left" color="#ffffffff" size={30} />
+          </TouchableOpacity>
+
+          <BottomModalButton type={bottomModalType} />
+        </View>
+      </Modal>
+
+      <Modal
+        style={styles.renameModal}
+        isVisible={renameModalVisible}
+        avoidKeyboard={true}
+      >
+        <View style={styles.moreModalInner}>
+          <TextInput editable />
+          <TouchableOpacity
+            style={[styles.backButton, { backgroundColor: colour.state }]}
+            onPress={() => {
+              setBottomModalVisible(false);
+              setSelectMode(false);
+            }}
+          >
+            <Text>{'Rename file'}</Text>
+          </TouchableOpacity>
+
+          <BottomModalButton type={bottomModalType} />
+        </View>
+      </Modal>
+    </View>
+  );
+};
 export default PdfView;
 
-PdfView.inStorybook = true;
-PdfView.fitScreen = false;
-//SettingsPage.scrollHeight = 844;
-
 const styles = StyleSheet.create({
-  settings: {
+  viewAllPage: {
     backgroundColor: '#ffffffff',
-    overflow: 'hidden',
-    flexShrink: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    marginLeft: 0,
     flexGrow: 1,
+    marginRight: 0,
+  },
+  viewAllTopBar: {
+    width: '100%',
+    flexShrink: 1,
+    backgroundColor: '#c4c4c4ff',
+    elevation: 2,
+    shadowColor: '#000000',
+    shadowRadius: 2.621621621621622,
+    shadowOpacity: 0.2173913043478261,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
     alignItems: 'center',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    top: 0,
+    zIndex: 999,
+    minHeight: 88,
   },
   big_title: {
     color: '#344053ff',
@@ -133,30 +380,183 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     fontStyle: 'normal',
     fontFamily: 'System' /* Jaldi */,
-    paddingHorizontal: 0,
-    paddingVertical: 0
   },
   big_title_box: {
     alignItems: 'flex-start',
     justifyContent: 'center',
     paddingLeft: 15,
-    height: '10%',
+    paddingTop: 5,
+    height: '5%',
     width: '100%',
-    minHeight: 28
+    minHeight: 28,
   },
-  settingsBody: {
+  searchBarGroup: {
     width: '85%',
+    flexShrink: 1,
+    margin: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    minHeight: 38,
+  },
+  searchInput: {
+    backgroundColor: '#ffffffff',
+    borderRadius: 8,
+    borderStyle: 'solid',
+    borderColor: '#d0d5ddff',
+    borderWidth: 1,
+    elevation: 2,
+    shadowColor: '#000000',
+    shadowRadius: 2.621621621621622,
+    shadowOpacity: 0.2173913043478261,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    color: '#667084ff',
+    textAlign: 'left',
+    letterSpacing: 0,
+    lineHeight: 24,
+    fontSize: 16,
+    fontWeight: '400',
+    fontStyle: 'normal',
+    fontFamily: 'System' /* Inter */,
+    padding: 5,
+    flexGrow: 1,
+  },
+  searchIconFrame: {
+    resizeMode: 'contain',
+    marginHorizontal: 10,
+  },
+  recentPdfTiles: {
+    height: '70%',
+    paddingLeft: 15,
+    paddingRight: 15,
+    overflow: 'visible',
+  },
+  viewAllBottomBar: {
+    width: '100%',
+    flexDirection: 'row',
+    flexShrink: 1,
+    backgroundColor: '#c4c4c4ff',
+    //shadowColor: 'transparent' /* cannot find mapping from CSS: 0px -4px 4px 0px rgba(0,0,0,0.09803921568627451), https://ethercreative.github.io/react-native-shadow-generator/ */
+    borderRadius: 5,
+    borderStyle: 'solid',
+    borderColor: '#d0d5ddff',
+    borderWidth: 1,
+    elevation: 2,
+    shadowColor: '#000000',
+    shadowRadius: 2.621621621621622,
+    shadowOpacity: 0.2173913043478261,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    justifyContent: 'center',
+  },
+  backButton: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  moreButton: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  orderByGroup: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'flex-end',
+    marginVertical: 5,
+  },
+  orderByLabel: {
+    color: '#344053ff',
+    textAlign: 'center',
+    letterSpacing: 0,
+    lineHeight: 20,
+    fontSize: 14,
+    fontWeight: '500',
+    fontStyle: 'normal',
+    fontFamily: 'System' /* Inter */,
+    padding: 3,
+    flexShrink: 1,
+    width: 50,
+  },
+  orderByDropdown: {
+    flexShrink: 1,
+    backgroundColor: '#ffffffff',
+    borderRadius: 8,
+    borderStyle: 'solid',
+    borderColor: '#d0d5ddff',
+    borderWidth: 1,
+    elevation: 2,
+    shadowColor: '#000000',
+    shadowRadius: 2.621621621621622,
+    shadowOpacity: 0.2173913043478261,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    flexDirection: 'row',
+    marginVertical: 5,
+    width: 65,
+  },
+  orderByDropdownText: {
+    color: '#667084ff',
+    textAlign: 'left',
+    letterSpacing: 0,
+    lineHeight: 18,
+    fontSize: 16,
+    fontWeight: '400',
+    fontStyle: 'normal',
+    fontFamily: 'System' /* Inter */,
+    padding: 10,
+  },
+  orderByDropDownText_box: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  orderByDropdownStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 80,
+    borderRadius: 8,
+  },
+  orderByDropdownTextStyle: {
+    color: '#667084ff',
+    textAlign: 'left',
+    letterSpacing: 0,
+    lineHeight: 18,
+    fontSize: 16,
+    fontWeight: '400',
+    fontStyle: 'normal',
+    fontFamily: 'System' /* Inter */,
+    padding: 10,
+  },
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  moreModalInner: {
+    width: '45%',
+    flexShrink: 1,
     backgroundColor: '#f5f5f5ff',
     borderRadius: 7,
-    flexDirection: 'column'
+    flexDirection: 'column',
+    borderWidth: 1,
+    borderColor: '#667084ff',
+    opacity: 1,
   },
-  settingsButton: {
+  moreModalButton: {
     flexGrow: 1,
     height: '8%',
     alignItems: 'center',
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
-  settingsButtonContent: {
+  moreModalButtonContent: {
     flexGrow: 1,
     flexDirection: 'row',
     alignItems: 'center',
@@ -164,11 +564,11 @@ const styles = StyleSheet.create({
     //padding: 5
   },
   iconContainer: {
-    width: '15%',
+    width: '40%',
     height: '100%',
-    alignItems: 'center'
+    alignItems: 'center',
   },
-  settingsButtonText: {
+  moreModalButtonText: {
     color: '#344053ff',
     textAlign: 'center',
     letterSpacing: 0,
@@ -178,21 +578,21 @@ const styles = StyleSheet.create({
     fontStyle: 'normal',
     fontFamily: 'System' /* Inter */,
   },
-  settingsButtonText_box: {
-    flexShrink: 1
+  moreModalButtonText_box: {
+    flexShrink: 1,
   },
-  settingsButtonDivider: {
+  moreModalButtonDivider: {
     backgroundColor: '#d0d5ddff',
     height: 1,
     width: '87%',
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
-  backButton: {
-    width: '15%',
-    height: '15%',
-    alignItems: 'center',
+  modalBottomBar: {
+    width: '100%',
+    height: '13%',
+    flexDirection: 'row',
+    //flexShrink: 1,
     justifyContent: 'center',
-    position: 'absolute',
-    bottom: 0
-  }
+    //alignSelf: 'flex-end'
+  },
 });
