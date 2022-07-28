2a07e592a5bb688f4a7e90d0437f8d9308a547db
Klara Meyer <u20428082@tuks.co.za>
2022-07-28 02:20:08 +0200

    update

--- libs/api/summarise-text/service/src/lib/commands/handlers/summarise-summarise-text.handlers.ts
+++ libs/api/summarise-text/service/src/lib/commands/handlers/summarise-summarise-text.handlers.ts
@@ -1,5 +1,6 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { SummariseCommand } from '../impl/summarise-summarise-text.command';
+import { ajax, css } from "jquery";
 // import { MongoDBAccess } from '@conversation-catcher/api/pdf-manager/repository/data-access';
 // import { HttpService } from '@nestjs/axios';
 // import { lastValueFrom, map, tap } from 'rxjs';
@@ -9,7 +10,14 @@ export class SummariesHandler implements ICommandHandler<SummariseCommand> {
   // constructor() {}
 
   async execute({ text }: SummariseCommand) {
-    // Place AI service here.
-    return 'summarised ' + text;
+
+    ajax({
+      type: "POST",
+      url: "~/pythoncode.py",
+      data: { param: text}
+    }).done(function( o ) {
+      return 'summarised ' + text;
+    });
+    
   }
 }
