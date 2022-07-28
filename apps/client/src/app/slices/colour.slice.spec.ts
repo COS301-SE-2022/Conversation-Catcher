09142ec53f5f9e21a256f018f6dfed669169df52
u20437863-James-Butler <u20437863@tuks.co.za>
2022-07-28 13:55:01 +0200

    Update colour.slice.spec.ts

--- apps/client/src/app/slices/colour.slice.spec.ts
+++ apps/client/src/app/slices/colour.slice.spec.ts
@@ -3,9 +3,9 @@ import { useSelector } from 'react-redux';
 
 describe('colour reducer', () => {
   it('retrieves stored colour',()=>{
-    expect(getColour).toBeCalled()
+    expect(getColour()).toBeCalled()
   }),
   it('returns a string containing #',() =>{
-    expect(getColour).toContain("#")
+    expect(getColour()).toContain("#")
   })
 });
