55a47ac483851b2265f9e9ecf2214baed6cde7e0
Xander <u20465026@tuks.co.za>
2022-07-29 01:51:38 +0200

    Other http request

--- libs/api/summarise-text/service/src/lib/commands/handlers/summarise-summarise-text.handlers.ts
+++ libs/api/summarise-text/service/src/lib/commands/handlers/summarise-summarise-text.handlers.ts
@@ -2,24 +2,43 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { SummariseCommand } from '../impl/summarise-summarise-text.command';
 //import { ajax, css } from "jquery";
 // import { MongoDBAccess } from '@conversation-catcher/api/pdf-manager/repository/data-access';
-// import { HttpService } from '@nestjs/axios';
-// import { lastValueFrom, map, tap } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom, map, tap } from 'rxjs';
 
 @CommandHandler(SummariseCommand)
 export class SummariesHandler implements ICommandHandler<SummariseCommand> {
-  // constructor() {}
+  constructor(private httpService: HttpService) {}
 
   async execute({ text }: SummariseCommand) {
+    // fetch('http://127.0.0.1:5000/summarise', {
+    //   method: 'POST',
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     text: text,
+    //   }),
+    // }).then((response) => { return response; });
 
-    fetch('http://127.0.0.1:5000/summarise', {
-      method: 'POST',
+    //Add elements to the correct user
+
+    const data = JSON.stringify({
+      text: text,
+    });
+
+    const config = {
+      method: 'post',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        text: text,
-      }),
-    }).then((response) => { return response; });
+    };
+
+    return await lastValueFrom(
+      this.httpService
+        .post('http://127.0.0.1:5000/summarise', data, config)
+        .pipe(map((res) => res.data))
+    );
   }
 }
