1a807f5a68b49c61c820a28bf955adaafaf49805
u20437863-James-Butler <u20437863@tuks.co.za>
2022-08-04 21:04:23 +0200

    Update user.slice.ts

--- apps/client/src/app/slices/user.slice.ts
+++ apps/client/src/app/slices/user.slice.ts
@@ -1,28 +1,30 @@
 import {
   createSlice,
 } from '@reduxjs/toolkit';
-import { act } from '@testing-library/react-native';
 
 export const USER = 'user';
 
+const initState = {
+  email:"",
+  colour:"#3f89beff",
+  pdfs: []
+}
+
 export const userSlice = createSlice({
   name: USER,
-  initialState: {
-    email:"",
-    colour:"#",
-    pdfs: []
-  },
+  initialState: initState,//Set the store to the initial state
   reducers: {
     setUser: (state,action) => {state = action.payload;return state},
     setEmail: (state,action) => {state.email = action.payload;return state},
     setColour: (state,action) => {state.colour = action.payload;return state},
     addDF: (state,action) => {state.pdfs.push(action.payload)},
-    removePDF: (state,action) => {
+    removePDF: (state,action) => {//remove the pdf that has the name provided in the action
       state.pdfs.forEach((item,index) => {
         if (item === action.payload) state.pdfs.splice(index,1)
       })
       return state;
-    }
+    },
+    clearUser: (state,action) => {state = initState}
   },
 });
 
