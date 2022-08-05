6417b5a678850e4aeb60d21377208891891be44e
u20437863-James-Butler <u20437863@tuks.co.za>
2022-08-05 16:04:11 +0200

    Fix spec files

--- libs/client/ui/src/lib/shared-components/pdf-tile/pdf-tile.spec.js
+++ libs/client/ui/src/lib/shared-components/pdf-tile/pdf-tile.spec.js
@@ -4,7 +4,7 @@ import PdfTile from './pdf-tile';
 import { create } from 'react-test-renderer';
 import { Provider } from 'react-redux';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import {reducer} from 'apps/client/src/app/slices/colour.slice';
+import {reducer} from 'apps/client/src/app/slices/user.slice';
 import { configureStore } from '@reduxjs/toolkit';
 
 describe('PdfTile', () => {
