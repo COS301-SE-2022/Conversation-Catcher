38bb4b39b065b909324d923eb4e5623e3dafdeb9
Klara Meyer <u20428082@tuks.co.za>
2022-08-09 17:11:38 +0200

    Update speech-to-text.py

--- libs/api/speech-to-text/deepspeech/speech-to-text.py
+++ libs/api/speech-to-text/deepspeech/speech-to-text.py
@@ -13,3 +13,14 @@ try:
     from shhlex import quote
 except ImportError:
     from pipes import quote
+
+def convert_samplerate(audio_path, desired_sample_rate):
+    sox_cmd = 'sox {} --type raw --bits 16 --channels 1 --rate {} --encoding signed-integer --endian little --compression 0.0 --no-dither - '.format(quote(audio_path), desired_sample_rate)
+    try:
+        output = subprocess.check_output(shlex.split(sox_cmd), stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError('SoX returned non-zero status: {}'.format(e.stderr))
+    except OSError as e:
+        raise OSError(e.errno, 'SoX not found, use {}hz files or install it: {}'.format(desired_sample_rate, e.strerror))
+
+    return desired_sample_rate, np.frombuffer(output, np.int16)
