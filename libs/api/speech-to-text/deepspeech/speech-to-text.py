c6474504044f29701c2ea2f50ee8e58542c0db0b
Klara Meyer <u20428082@tuks.co.za>
2022-08-09 17:15:49 +0200

    Update speech-to-text.py

--- libs/api/speech-to-text/deepspeech/speech-to-text.py
+++ libs/api/speech-to-text/deepspeech/speech-to-text.py
@@ -39,3 +39,23 @@ def words_from_candidate_transcript(metadata):
             if len(word) == 0:
                 # Log the start time of the new word
                 word_start_time = token.start_time
+                
+            word = word + token.text
+        # Word boundary is either a space or the last character in the array
+        if token.text == " " or i == len(metadata.tokens) - 1:
+            word_duration = token.start_time - word_start_time
+
+            if word_duration < 0:
+                word_duration = 0
+
+            each_word = dict()
+            each_word["word"] = word
+            each_word["start_time"] = round(word_start_time, 4)
+            each_word["duration"] = round(word_duration, 4)
+
+            word_list.append(each_word)
+            # Reset
+            word = ""
+            word_start_time = 0
+
+    return word_list
