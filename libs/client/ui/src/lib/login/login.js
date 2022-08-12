88cc76c69e0f876ec83b94e53c229f0131a5caf9
Xander <u20465026@tuks.co.za>
2022-08-12 09:11:17 +0200

    ✨: Login email and password error checking

--- libs/client/ui/src/lib/login/login.js
+++ libs/client/ui/src/lib/login/login.js
@@ -27,8 +27,10 @@ export const Login = ({ navigation }) => {
   // console.log('Colour:' + colourState);
   const [showMailHint, setShowMailHint] = useState(false);
   const [showPasswordHint, setShowPasswordHint] = useState(false);
+  const [failedLogin, setFailedLogin] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('Invalid login details');
 
   // dispatch(setUser({ colour: '#ffff', pdfs: [], email: '' }));
 
@@ -56,6 +58,7 @@ export const Login = ({ navigation }) => {
       return null;
     }
   }
+
   function PasswordHint() {
     if (showPasswordHint) {
       return (
@@ -68,7 +71,14 @@ export const Login = ({ navigation }) => {
     }
   }
 
-  // console.log(useSelector(selectUser)); //test statement
+  function InvalidAuth() {
+    if (!failedLogin) return null;
+    return (
+      <View style={styles.hintText_box}>
+        <Text style={styles.errorText}>{errorMessage}</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.logInPage}>
@@ -76,6 +86,7 @@ export const Login = ({ navigation }) => {
         <Text style={styles.big_title}>{'Log in to your account'}</Text>
       </View>
       <View style={styles.inputsGroup}>
+        <InvalidAuth />
         <View style={styles.inputsItem}>
           <View style={styles.inputLabel_box}>
             <Text style={styles.inputLabel}>{'Email'}</Text>
@@ -93,6 +104,7 @@ export const Login = ({ navigation }) => {
                 style={styles.inputText}
                 placeholder="johnsmith@gmail.com"
                 underlineColorAndroid="transparent"
+                value={email}
                 onChangeText={(text) => {
                   setEmail(text);
                 }}
@@ -126,6 +138,7 @@ export const Login = ({ navigation }) => {
                 style={styles.inputText}
                 placeholder="*******************"
                 underlineColorAndroid="transparent"
+                value={password}
                 onChangeText={(text) => {
                   setPassword(text);
                 }}
@@ -156,27 +169,38 @@ export const Login = ({ navigation }) => {
           { borderColor: colourState },
         ]}
         onPress={() => {
+          //Check that email and password is not empty
+          if (email === '') {
+            setFailedLogin(true);
+            setErrorMessage('Email is required');
+            return;
+          }
+          if (password === '') {
+            setFailedLogin(true);
+            setErrorMessage('Password is required');
+            return;
+          }
           auth()
-            .signInWithEmailAndPassword(email.trim().toLowerCase(), password.trim())
+            .signInWithEmailAndPassword(
+              email.trim().toLowerCase(),
+              password.trim()
+            )
             .then(async () => {
+              setFailedLogin(false);
               navigation.navigate('Home');
-              var queryRes = (await getUser({ variables: { email: email.trim().toLowerCase() } }))
-                .data.getUser;
+              var queryRes = (
+                await getUser({
+                  variables: { email: email.trim().toLowerCase() },
+                })
+              ).data.getUser;
               console.log(queryRes);
               dispatch(setUser(queryRes));
             })
             .catch((error) => {
-              if (error.code === 'auth/invalid-password') {
-                console.log('The provided password is invalid!');
-              }
+              setFailedLogin(true);
+              setErrorMessage('Invalid login details');
+              setPassword('');
             });
-          // console.log(email);
-          // console.log(password);
-          // var queryRes = (await getUser({ variables: { email: email } })).data
-          //   .getUser;
-          // console.log(queryRes);
-          // dispatch(setUser(queryRes));
-          // navigation.navigate('Home');
         }}
       >
         <View style={styles.logInButtonLabel_box}>
@@ -397,5 +421,15 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
     justifyContent: 'flex-start',
     margin: 5,
+  },
+  errorText: {
+    color: '#e11e22',
+    textAlign: 'left',
+    letterSpacing: 0,
+    lineHeight: 20,
+    fontSize: 14,
+    fontWeight: '400',
+    fontStyle: 'normal',
+    fontFamily: 'System' /* Inter */,
   },
 });
