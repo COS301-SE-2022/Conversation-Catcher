9eb4db65d304c2810ae926b6571aa77fc0632e14
Klara Meyer <u20428082@tuks.co.za>
2022-08-29 15:48:02 +0200

    Fixed speech summariser api connection

--- libs/api/summarise-text/huggingface/summariseText.py
+++ libs/api/summarise-text/huggingface/summariseText.py
@@ -2,9 +2,10 @@ from transformers import pipeline
 
 class summariseText:
 
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    def summarise(self, input_text):
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
-    def summarise(input_text):
-        max = len(input_text)/255
+        max = len(input_text)/2
         min = len(input_text)/10
+
         return summarizer(input_text, max_length=130, min_length=30, do_sample=False)
