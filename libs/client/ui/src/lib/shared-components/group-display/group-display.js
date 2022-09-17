5fc934609aa642983f4b46fe7868631c4e091506
Klara Meyer <u20428082@tuks.co.za>
2022-09-17 11:34:11 +0200

    Update group-display.js

--- libs/client/ui/src/lib/shared-components/group-display/group-display.js
+++ libs/client/ui/src/lib/shared-components/group-display/group-display.js
@@ -2,13 +2,13 @@ import { gql, useMutation, useQuery } from '@apollo/client';
 import React, { useImperativeHandle, forwardRef, useState } from 'react';
 import { Text, ScrollView, StyleSheet, DeviceEventEmitter } from 'react-native';
 import Loading from '../loading/loading';
-// import LocalPdfsAccess from '../local-pdfs-access/local-pdfs-access';
+// import LocalGroupsAccess from '../local-groups-access/local-groups-access';
 import GroupTile from '../group-tile/group-tile';
-import pdfLocalAccess from '../local-pdfs-access/local-pdfs-access';
+import groupLocalAccess from '../local-groups-access/local-groups-access';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { selectEmail} from 'apps/client/src/app/slices/user.slice';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { selectPDFS, refillPDFs } from 'apps/client/src/app/slices/pdf.slice';
+import { selectGroups, refillGroups } from 'apps/client/src/app/slices/group.slice';
 import { useSelector, useDispatch } from 'react-redux';
 
 export function GroupDisplay({ navigation, selectMode }, ref) {
@@ -16,7 +16,7 @@ export function GroupDisplay({ navigation, selectMode }, ref) {
   const [didReload, setDidReload] = useState(true);
   const [isLoaded, setIsLoaded] = useState(false);
   const emailState = useSelector(selectEmail);
-  const localPDFs = useSelector(selectPDFS);
+  const localgroups = useSelector(selectGroups);
   const dispatch = useDispatch();
   //Expose refresh function to parent(View-all page)
   useImperativeHandle(ref, () => ({
@@ -29,29 +29,29 @@ export function GroupDisplay({ navigation, selectMode }, ref) {
   //Listen to when to update page
   DeviceEventEmitter.addListener('updatePage', () => setDidReload(!didReload));
   //graphql syntax trees
-  const GET_USER_PDFS = gql`
+  const GET_USER_groupS = gql`
     query getForUser($email: String!) {
-      getPDFs(id: $email) {
+      getgroups(id: $email) {
         id
         name
         creationDate
         downloaded
-        #pdf
+        #group
         text
       }
     }
   `;
-  const { data, loading, error } = useQuery(GET_USER_PDFS, {
+  const { data, loading, error } = useQuery(GET_USER_groupS, {
     variables: { email: emailState },
   });
-  // console.log('GetPdfs');
+  // console.log('GetGroups');
   // console.log(data);
   // console.log(loading);
   // console.log(error);
   if (loading)
     return (
-      <ScrollView style={styles.recentPdfTiles}>
-        {localPDFs.map((item, key) => (
+      <ScrollView style={styles.recentGroupTiles}>
+        {localgroups.map((item, key) => (
         <GroupTile
           key={key}
           id={item.id}
@@ -61,7 +61,7 @@ export function GroupDisplay({ navigation, selectMode }, ref) {
           text={item.text}
           downloaded={item.downloaded}
           showCheck={selectMode}
-          pdfSource={'pdfRefresh'}
+          groupSource={'groupRefresh'}
           nav={navigation}
           refresh={setDidReload}
         />
@@ -72,46 +72,46 @@ export function GroupDisplay({ navigation, selectMode }, ref) {
 
   if (error)
     return (
-      <ScrollView style={styles.recentPdfTiles}>
+      <ScrollView style={styles.recentGroupTiles}>
         <Text>An error occured...</Text>
         <Text>{error[0]}</Text>
       </ScrollView>
     );
-  //If the pdf array is empty assign the result from the query
+  //If the group array is empty assign the result from the query
   //create deep copy of the returned data
   //Data is here in data if returned
   if (!isLoaded) {
-    pdfLocalAccess.clearPdfs();
-    for (let i = 0; i < data.getPDFs.length; i++) {
-      pdfLocalAccess.addPdf({
-        name: data.getPDFs[i].name,
-        creationDate: data.getPDFs[i].creationDate,
-        downloaded: data.getPDFs[i].downloaded,
-        pdf: data.getPDFs[i].pdf,
-        text: data.getPDFs[i].text,
-        id: data.getPDFs[i].id,
+    groupLocalAccess.clearGroups();
+    for (let i = 0; i < data.getgroups.length; i++) {
+      groupLocalAccess.addGroup({
+        name: data.getgroups[i].name,
+        creationDate: data.getgroups[i].creationDate,
+        downloaded: data.getgroups[i].downloaded,
+        group: data.getgroups[i].group,
+        text: data.getgroups[i].text,
+        id: data.getgroups[i].id,
       });
     }
     setIsLoaded(true);
-    //Update local pdf storage
-    //array of pdfs stored locally, selected from data to overwrite the slice
-    if (data.getPDFs[0].name !== "error"){
+    //Update local group storage
+    //array of groups stored locally, selected from data to overwrite the slice
+    if (data.getgroups[0].name !== "error"){
       let tempArray = [];
       var p;
-      for (p in pdfLocalAccess.getPdfs()){
-        if (data.getPDFs[p].downloaded === true){
-          tempArray.push(data.getPDFs[p]);
+      for (p in groupLocalAccess.getGroups()){
+        if (data.getgroups[p].downloaded === true){
+          tempArray.push(data.getgroups[p]);
         }
       }
-      dispatch(refillPDFs(tempArray));
+      dispatch(refillGroups(tempArray));
     }
   }
 
   
 
   return (
-    <ScrollView style={styles.recentPdfTiles}>
-      {pdfLocalAccess.getPdfs().map((item, key) => (
+    <ScrollView style={styles.recentGroupTiles}>
+      {groupLocalAccess.getGroups().map((item, key) => (
         <GroupTile
           key={key}
           id={item.id}
@@ -121,7 +121,7 @@ export function GroupDisplay({ navigation, selectMode }, ref) {
           text={item.text}
           downloaded={item.downloaded}
           showCheck={selectMode}
-          pdfSource={'pdfRefresh'}
+          groupSource={'groupRefresh'}
           nav={navigation}
           refresh={setDidReload}
         />
@@ -132,7 +132,7 @@ export function GroupDisplay({ navigation, selectMode }, ref) {
 export default forwardRef(GroupDisplay);
 
 const styles = StyleSheet.create({
-  recentPdfTiles: {
+  recentGroupTiles: {
     height: '70%',
     paddingLeft: 15,
     paddingRight: 15,
