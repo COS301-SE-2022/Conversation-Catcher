a5c007a24033864850880b60dc460d4b11381c3b
Klara Meyer <u20428082@tuks.co.za>
2022-09-17 11:36:01 +0200

    Create group.slice.ts

---
+++ apps/client/src/app/slices/group.slice.ts
@@ -0,0 +1,57 @@
+import {
+  createSlice,
+} from '@reduxjs/toolkit';
+
+export const PDF = 'pdf';
+
+export const pdfSlice = createSlice({
+  name: PDF,
+  initialState: [],
+  reducers: {
+    addPDF: (state,action) => {
+      state.push(action.payload);
+      return state;
+    },
+    removePDF: (state,action) => {
+      state.forEach((item,index) => {
+        if (item.id === action.payload.id) state.splice(index,1)
+      })
+      return state;
+    },
+    clearPDFs: (state,action) => {
+      state = [];
+      return state;
+    },
+    refillPDFs: (state,action) => {
+      state = action.payload;
+      return state;
+    },
+    toggleDown: (state,action) => {
+      let notIn = true;
+      state.forEach((item,index) => {
+        if (item.id === action.payload.id){
+          state.splice(index,1);
+          notIn = false;
+        }
+      })
+      if (notIn) {
+        state.push({...action.payload,downloaded:{notIn}});
+      };
+      return state;
+    },
+    changeName: (state,action) => {
+      state.forEach((item,index) => {
+        if (item.id === action.payload.id){
+          state[index].name = action.payload.name;
+        }
+      })
+      return state;
+    }
+  },
+});
+
+export const {actions, reducer} = pdfSlice;
+
+export const {addPDF, removePDF, clearPDFs, refillPDFs, toggleDown, changeName} = actions;
+
+export const selectPDFS = (state) => state.pdf;
