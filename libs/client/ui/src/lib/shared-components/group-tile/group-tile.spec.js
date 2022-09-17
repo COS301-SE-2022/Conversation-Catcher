7a802ec925dfc4078121d8b23fe5b24412bfe699
Klara Meyer <u20428082@tuks.co.za>
2022-09-17 11:20:07 +0200

    Update group-tile.spec.js

--- libs/client/ui/src/lib/shared-components/group-tile/group-tile.spec.js
+++ libs/client/ui/src/lib/shared-components/group-tile/group-tile.spec.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import PdfTile from './pdf-tile';
+import GroupTile from './pdf-tile';
 import { create } from 'react-test-renderer';
 import { Provider } from 'react-redux';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import {reducer} from 'apps/client/src/app/slices/user.slice';
 import { configureStore } from '@reduxjs/toolkit';
 
-describe('PdfTile', () => {
+describe('GroupTile', () => {
   const store = configureStore({
     reducer: {
         user:reducer
@@ -15,7 +15,7 @@ describe('PdfTile', () => {
   })
   const TestComponent = () => (
     <Provider store = { store }>
-        <PdfTile />
+        <GroupTile />
     </Provider>
 )
   it('should render successfully', () => {//Wrap component in provider
