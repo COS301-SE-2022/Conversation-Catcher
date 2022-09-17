cdc8dc0c92ce609c46b97463ee6ea055bf741cb0
Klara Meyer <u20428082@tuks.co.za>
2022-09-17 11:36:03 +0200

    Create group.slice.spec.ts

---
+++ apps/client/src/app/slices/group.slice.spec.ts
@@ -0,0 +1,10 @@
+import { selectPDFS,addPDF,removePDF} from './pdf.slice';
+
+describe('pdf reducer', () => {
+  it('returns an array',()=>{
+    expect(selectPDFS({pdf:[]}).length).toEqual(0)
+  });
+  it('adds a pdf',()=>{
+    expect(addPDF({name:"Bob"}).payload.name).toContain("Bob")
+  });
+})
