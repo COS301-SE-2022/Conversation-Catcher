f2aa8c27255f2cf953d2c5c48bc0a83e893d2806
Klara Meyer <u20428082@tuks.co.za>
2022-09-20 11:18:20 +0200

    Adding name generation

---
+++ libs/api/generate-names/api/jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  displayName: 'api-summarise-text-api',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/api/summarise-text/api',
+};
