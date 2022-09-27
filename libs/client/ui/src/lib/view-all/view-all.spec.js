276e8a59b3c017da172cecd9dfca615921e8acf5
RRduToit <u20450533@Tuks.co.za>
2022-09-27 13:05:41 +0200

    Update view-all.spec.js

--- libs/client/ui/src/lib/view-all/view-all.spec.js
+++ libs/client/ui/src/lib/view-all/view-all.spec.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 import ViewAll from './view-all';
 import { create } from 'react-test-renderer';
-
+//import { create } from 'react-test-renderer';
+import { configureStore } from '@reduxjs/toolkit';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import reducer from '../../../../../../apps/client/src/app/slices/user.slice';
+import { Provider } from 'react-redux';
 
 describe('ViewAll', () => {
-  // const client = jest.fn();
-  // ApolloClient.Link.HttpLink.HttpOptions.Js_.t_fetch = 'default';
-  // const client = new ApolloClient({
-  //   uri: 'http://localhost:3333/graphql',
-  //   link: new HttpLink({ uri: '/graphql', fetch }),
-  //   cache: new InMemoryCache(),
-  // });
-  it('should render successfully', () => {
-  //   const { container } = render(<ViewAll params={client}/>);
-  //   expect(container).toBeTruthy();
-      expect(true).toEqual(true);
+  const store = configureStore({
+    reducer: {
+      user:reducer
+    },
+  });
+  const TestComponent = () => (
+    <Provider store={store}>
+      <ViewAll />
+    </Provider>
+  );
+
+  render(TestComponent);
+
+  //new email
+  const emailInput = screen.getByPlaceholderText("johnsmith@gmail.com");
+  expect(emailInput).toBeTruthy();
+  
+  const textToEnterEmail = 'random@random.com';
+  fireEvent.changeText(emailInput, textToEnterEmail);
+
+  //password
+  const passwordInput = screen.getByPlaceholderText("*******************");
+  expect(passwordInput).toBeTruthy();
+  
+  const textToEnterPassword = '123qwe123#';
+  fireEvent.changeText(passwordInput, textToEnterPassword);
+
+  // Submit
+  fireEvent.press(screen.ByLabelText("Log in"));
+  
+  it('test email', () => {
+    const emailState = store.getState().email;
+    expect(emailState).toEqual('random@random.com');
   });
 
-  // const tree = create(<ViewAll />);
-  // test('snapshot', () => {
-  //   expect(tree).toMatchSnapshot();
-  // });
+  it('test password', () => {
+    const passwordState = store.getState().password;
+    expect(passwordState).toEqual('123qwe123#');
+  });
 });
