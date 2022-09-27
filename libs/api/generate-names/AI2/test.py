3fee33b5cdc2200ce8fd9ed69d6df82f6c3cccd5
Klara Meyer <u20428082@tuks.co.za>
2022-09-27 19:42:28 +0200

    Working name generation

--- libs/api/generate-names/AI2/test.py
+++
@@ -1,6 +0,0 @@
-from nameGenerator import nameGenerator
-
-generator = nameGenerator()
-input_text = "Duck is the common name for numerous species of waterfowl in the family Anatidae. Ducks are generally smaller and shorter-necked than swans and geese, which are members of the same family. Divided among several subfamilies, they are a form taxon; they do not represent a monophyletic group (the group of all descendants of a single common ancestral species), since swans and geese are not considered ducks. Ducks are mostly aquatic birds, and may be found in both fresh water and sea water."
-generated_name = generator.generateName(input_text)
-print("Generated name: ", generated_name)
