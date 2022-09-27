6fcb07b62aaa552e54170901bbcf3b4416d46f6c
RRduToit <u20450533@Tuks.co.za>
2022-09-27 12:03:25 +0200

    Update change-email.spec.js

--- libs/client/ui/src/lib/change-email/change-email.spec.js
+++ libs/client/ui/src/lib/change-email/change-email.spec.js
@@ -5,66 +5,9 @@ import { configureStore } from '@reduxjs/toolkit';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import reducer from '../../../../../../apps/client/src/app/slices/user.slice';
 import { Provider } from 'react-redux';
-
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  selectColour,
-  selectUser,
-  setEmail,
-} from '@conversation-catcher/app/client/src/app/slices/user.slice';
-
 import { screen, fireEvent } from '@testing-library/react-native';
-/*import { QuestionsBoard } from '../QuestionsBoard';
 
-test('form submits two answers', () => {
-  const allQuestions = ['q1', 'q2'];
-  const mockFn = jest.fn();
-
-  render(<QuestionsBoard questions={allQuestions} onSubmit={mockFn} />);
-
-  const answerInputs = screen.getAllByLabelText('answer input');
-
-  fireEvent.changeText(answerInputs[0], 'a1');
-  fireEvent.changeText(answerInputs[1], 'a2');
-  fireEvent.press(screen.getByText('Submit'));
-
-  expect(mockFn).toBeCalledWith({
-    '1': { q: 'q1', a: 'a1' },
-    '2': { q: 'q2', a: 'a2' },
-  });
-});
-
-test('fire changeText event', () => {
-  const colourState = useSelector(selectColour);
-  const user = useSelector(selectUser);
-  const [showMailHint, setShowMailHint] = useState(false);
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [failedText, setFailedText] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [email, setNewEmail] = useState('');
-  const [checkEmail, setCheckEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const dispatch = useDispatch();
-  const store = configureStore({
-    reducer: {
-      user:reducer
-    },
-  });
-  const onEventMock = jest.fn();
-  render(
-    // MyComponent renders TextInput which has a placeholder 'Enter details'
-    // and with `onChangeText` bound to handleChangeText
-    <Provider store={store}>
-      <ChangeEmail />
-    </Provider>
-  );
-
-  fireEvent(screen.getByPlaceholderText('change'), 'onChangeText', 'ab');
-  expect(onEventMock).toHaveBeenCalledWith('ab');
-});*/
-
-describe('ChangeEmail', () => {
+/*describe('ChangeEmail', () => {
   const store = configureStore({
     reducer: {
       user:reducer
@@ -80,33 +23,54 @@ describe('ChangeEmail', () => {
     expect(container).toBeTruthy();
     expect(true).toEqual(true);
   });
-});
+});*/
 
-test('fire changeText event', () => {
-  const colourState = useSelector(selectColour);
-  const user = useSelector(selectUser);
-  const [showMailHint, setShowMailHint] = useState(false);
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [failedText, setFailedText] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [email, setNewEmail] = useState('');
-  const [checkEmail, setCheckEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const dispatch = useDispatch();
+describe('ChangeEmail', () => {
   const store = configureStore({
     reducer: {
       user:reducer
     },
   });
-  const onEventMock = jest.fn();
-  render(
-    // MyComponent renders TextInput which has a placeholder 'Enter details'
-    // and with `onChangeText` bound to handleChangeText
+  const TestComponent = () => (
     <Provider store={store}>
       <ChangeEmail />
     </Provider>
   );
 
-  fireEvent(screen.getByPlaceholderText('change'), 'onChangeText', 'ab');
-  expect(onEventMock).toHaveBeenCalledWith('ab');
+  render(TestComponent);
+
+  const input = screen.getByPlaceholderText(/repository/i);
+  expect(input).toBeTruthy();
+
+  const textToEnter = 'This is a random element';
+  fireEvent.changeText(input, textToEnter);
+  fireEvent.press(screen.getByText('Submit form'));
+
+  //new email
+  const newEmailInput = screen.getByText("johnsmith@gmail.com");
+  expect(newEmailInput).toBeTruthy();
+  
+  const textToEnterNewEmail = 'random@random.com';
+  fireEvent.changeText(newEmailInput, textToEnterNewEmail);
+
+  // setCheckEmail
+  const checkEmailInput = screen.getByText("johnsmith@gmail.com");
+  expect(checkEmailInput).toBeTruthy();
+  
+  const textToEnterCheckEmail = 'random@random.com';
+  fireEvent.changeText(newEmailInput, textToEnterCheckEmail);
+
+  // setPassword
+  const passwordInput = screen.getByText("*********");
+  expect(passwordInput).toBeTruthy();
+  
+  const textToEnterPassword = '123qwe123#';
+  fireEvent.changeText(passwordInput, textToEnterPassword);
+
+  // Submit
+  fireEvent.press(screen.getByText('Change email'));
+
+  const emailState = store.getState().email;
+
+  expect(emailState).toEqual('random@random.com');
 });
