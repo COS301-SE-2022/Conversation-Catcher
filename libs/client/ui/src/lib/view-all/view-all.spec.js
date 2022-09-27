66a6344d76b72ef9fc8a796f48a37879e1fb334b
RRduToit <u20450533@Tuks.co.za>
2022-09-27 13:16:52 +0200

    Update view-all.spec.js

--- libs/client/ui/src/lib/view-all/view-all.spec.js
+++ libs/client/ui/src/lib/view-all/view-all.spec.js
@@ -8,6 +8,26 @@ import { configureStore } from '@reduxjs/toolkit';
 import reducer from '../../../../../../apps/client/src/app/slices/user.slice';
 import { Provider } from 'react-redux';
 
+// describe('ViewAll', () => {
+//   // const client = jest.fn();
+//   // ApolloClient.Link.HttpLink.HttpOptions.Js_.t_fetch = 'default';
+//   // const client = new ApolloClient({
+//   //   uri: 'http://localhost:3333/graphql',
+//   //   link: new HttpLink({ uri: '/graphql', fetch }),
+//   //   cache: new InMemoryCache(),
+//   // });
+//   it('should render successfully', () => {
+//   //   const { container } = render(<ViewAll params={client}/>);
+//   //   expect(container).toBeTruthy();
+//       expect(true).toEqual(true);
+//   });
+
+//   // const tree = create(<ViewAll />);
+//   // test('snapshot', () => {
+//   //   expect(tree).toMatchSnapshot();
+//   // });
+// });
+
 describe('ViewAll', () => {
   const store = configureStore({
     reducer: {
