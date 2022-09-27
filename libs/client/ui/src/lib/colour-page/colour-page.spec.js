e666562dde611a541e2803d5d64038054a873ea1
RRduToit <u20450533@Tuks.co.za>
2022-09-27 12:58:31 +0200

    Update colour-page.spec.js

--- libs/client/ui/src/lib/colour-page/colour-page.spec.js
+++ libs/client/ui/src/lib/colour-page/colour-page.spec.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 import ColourPage from './colour-page';
 import { Provider } from 'react-redux';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import reducer from '../../../../../../apps/client/src/app/slices/user.slice';
 import { configureStore } from '@reduxjs/toolkit';
 
+// describe('ColourPage', () => {
+//   const store = configureStore({
+//     reducer: {
+//       user:reducer
+//     }
+//   })
+//   const TestComponent = () => (
+//     <Provider store = { store }>
+//         <ColourPage />
+//     </Provider>
+// )
+//   it('should render successfully', () => {
+//     // const { container } = render(<TestComponent />);
+//     // expect(container).toBeTruthy();
+//     expect(true).toEqual(true);
+//   });
+// });
+
 describe('ColourPage', () => {
   const store = configureStore({
     reducer: {
       user:reducer
-    }
-  })
-  const TestComponent = () => (
-    <Provider store = { store }>
-        <ColourPage />
-    </Provider>
-)
-  it('should render successfully', () => {
-    // const { container } = render(<TestComponent />);
-    // expect(container).toBeTruthy();
-    expect(true).toEqual(true);
+    },
   });
+  const TestComponent = () => (
+    <Provider store={store}>
+      <ColourPage />
+    </Provider>
+  );
+
+  render(TestComponent);
+
+  // Submit
+  //fireEvent.press(screen.ByLabelText("Register"));
+  
 });
