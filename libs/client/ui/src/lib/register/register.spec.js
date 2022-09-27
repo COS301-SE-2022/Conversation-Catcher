b9699f393b12d32d620531b6bf1574e26b2db2a9
RRduToit <u20450533@Tuks.co.za>
2022-09-27 12:48:02 +0200

    Update register.spec.js

--- libs/client/ui/src/lib/register/register.spec.js
+++ libs/client/ui/src/lib/register/register.spec.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 import Register from './register';
 import { configureStore } from '@reduxjs/toolkit';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import reducer from '../../../../../../apps/client/src/app/slices/user.slice';
 import { Provider } from 'react-redux';
 
+// describe('Register', () => {
+//   const store = configureStore({
+//     reducer: {
+//       user: reducer,
+//     },
+//   });
+//   const TestComponent = () => (
+//     <Provider store={store}>
+//       <Register />
+//     </Provider>
+//   );
+//   it('should render successfully', () => {
+//     // const { container } = render(<TestComponent />);
+//     // expect(container).toBeTruthy();
+//     expect(true).toEqual(true);
+//   });
+// });
+
 describe('Register', () => {
   const store = configureStore({
     reducer: {
-      user: reducer,
+      user:reducer
     },
   });
   const TestComponent = () => (
@@ -17,9 +34,33 @@ describe('Register', () => {
       <Register />
     </Provider>
   );
-  it('should render successfully', () => {
-    // const { container } = render(<TestComponent />);
-    // expect(container).toBeTruthy();
-    expect(true).toEqual(true);
+
+  render(TestComponent);
+
+  //new email
+  const emailInput = screen.getByPlaceholderText("johnsmith@gmail.com");
+  expect(emailInput).toBeTruthy();
+  
+  const textToEnterEmail = 'random@random.com';
+  fireEvent.changeText(emailInput, textToEnterEmail);
+
+  //password
+  const passwordInput = screen.getByPlaceholderText("*******************");
+  expect(passwordInput).toBeTruthy();
+  
+  const textToEnterPassword = '123qwe123#';
+  fireEvent.changeText(passwordInput, textToEnterPassword);
+
+  // Submit
+  fireEvent.press(screen.ByLabelText("Register"));
+  
+  it('test email', () => {
+    const emailState = store.getState().email;
+    expect(emailState).toEqual('random@random.com');
+  });
+
+  it('test password', () => {
+    const passwordState = store.getState().password;
+    expect(passwordState).toEqual('123qwe123#');
   });
 });
