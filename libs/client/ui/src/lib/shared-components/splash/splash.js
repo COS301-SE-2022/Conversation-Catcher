52457d3b9cc3040d5fe1b3f55798a018774781c0
u20437863-James-Butler <u20437863@tuks.co.za>
2022-09-28 12:06:29 +0200

    Splash screen

--- libs/client/ui/src/lib/shared-components/splash/splash.js
+++ libs/client/ui/src/lib/shared-components/splash/splash.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
 import Loading from '../loading/loading';
 import {
   selectUser,
@@ -12,13 +16,52 @@ export const Splash = ({navigation}) => {
     if (userPresent.email !== '') {
       navigation.navigate('Home');
     } else {
-      navigation.navigate('Login')
+      navigation.navigate('Login');
     }
   });
   return (
-    <View>
-      <Text>Welcome to Splash!</Text>
+    <View style={styles.splashScreen}>
+      <View style={styles.title}>
+        <Text style={styles.titleText}>Conversation Catcher</Text>
+      </View>
+      <View style={styles.icon}>
+        
+      </View>
+      <View style={styles.load}>
+        <Loading  width={100} height={100} load={true} text={"Loading"}/>
+      </View>
     </View>
   );
 }
 export default Splash;
+
+const styles = StyleSheet.create({
+  splashScreen: {
+    flex: 1,
+
+  },
+  title: {
+    alignContent: "center",
+    height: '30%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleText: {
+    color: '#3f89beff',
+    fontSize: 30,
+    fontWeight: "bold",
+    textShadowColor: "black",
+    textShadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    textShadowRadius: 2,
+
+  },
+  icon: {
+
+  },
+  load: {
+
+  }
+});
