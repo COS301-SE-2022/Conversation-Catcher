c992b1d26ea39adb5ec4293f8a4cfd8ab2f9db60
RRduToit <u20450533@Tuks.co.za>
2022-09-28 09:57:47 +0200

    Adding library to yarn for testing

--- libs/client/ui/src/lib/home/home.spec.js
+++ libs/client/ui/src/lib/home/home.spec.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { render } from '@testing-library/react-native';
 import Home from './home';
 import { create } from 'react-test-renderer';
@@ -25,3 +25,61 @@ describe('Home', () => {
 // describe('The actions that occur on button press', () => {
 //   it('should display the available colours')
 // });
+*/
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { render, screen, fireEvent } from '@testing-library/react-native';
+import AppNavigator from '../AppNavigator';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+describe('Testing react navigation', () => {
+  // test('page contains the header and 10 items', async () => {
+  //   const component = (
+  //     <NavigationContainer>
+  //       <AppNavigator />
+  //     </NavigationContainer>
+  //   );
+
+  //   render(component);
+
+  //   const header = await screen.findByText('List of numbers from 1 to 20');
+  //   const items = await screen.findAllByText(/Item number/);
+
+  //   expect(header).toBeTruthy();
+  //   expect(items.length).toBe(10);
+  // });
+
+  test('screen contains a button linking to the ViewAll page', async () => {
+    const component = (
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    );
+
+    render(component);
+    const button = await screen.findByText('My PDFs');
+
+    expect(button).toBeTruthy();
+  });
+
+  test('clicking on the button takes you to the ViewAll screen', async () => {
+    const component = (
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    );
+
+    render(component);
+    const oldScreen = screen.queryByText('Search');
+    const button = await screen.findByText('My PDFs');
+
+    expect(oldScreen).toBeTruthy();
+
+    fireEvent(button, 'press');
+    const newScreen = await screen.queryByPlaceholderText('Recents');
+
+    expect(newScreen).toBeTruthy();
+  });
+});
