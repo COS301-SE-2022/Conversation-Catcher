25e193f6ce78daae8d53ed02525f7bad0e4a7aa0
Xander <u20465026@tuks.co.za>
2022-10-20 19:50:28 +0200

    ♻️: Set default value for redux

--- apps/client/src/app/slices/user.slice.ts
+++ apps/client/src/app/slices/user.slice.ts
@@ -1,39 +1,58 @@
-import {
-  createSlice,
-} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 export const USER = 'user';
 
 const initState = {
-  email:"",
-  colour:{},
-  pdfs: []
+  email: '',
+  colour: {
+    accent: '#3F89BE',
+    mode: '#FFFFFF',
+    bottom: '#E6ECF0',
+    low: '#B6BFC6',
+    high: '#667685',
+    top: '#344554',
+  },
+  pdfs: [],
 };
 
 export const userSlice = createSlice({
   name: USER,
-  initialState: initState,//Set the store to the initial state
+  initialState: initState, //Set the store to the initial state
   reducers: {
-    setUser: (state,action) => {state = action.payload;return state},         //set the state to the action's payload
-    setEmail: (state,action) => {state.email = action.payload;return state},  //set the email to the action's payload
-    setColour: (state,action) => {state.colour = action.payload;return state},//set the colour to the action's payload
-    addPDF: (state,action) => {state.pdfs.push(action.payload);return state;},              //add pdf to user
-    removePDF: (state,action) => {                                            //remove the pdf that has the name provided in the action
-      state.pdfs.forEach((item,index) => {
-        if (item === action.payload) state.pdfs.splice(index,1)
-      })
+    setUser: (state, action) => {
+      state = action.payload;
+      return state;
+    }, //set the state to the action's payload
+    setEmail: (state, action) => {
+      state.email = action.payload;
+      return state;
+    }, //set the email to the action's payload
+    setColour: (state, action) => {
+      state.colour = action.payload;
+      return state;
+    }, //set the colour to the action's payload
+    addPDF: (state, action) => {
+      state.pdfs.push(action.payload);
+      return state;
+    }, //add pdf to user
+    removePDF: (state, action) => {
+      //remove the pdf that has the name provided in the action
+      state.pdfs.forEach((item, index) => {
+        if (item === action.payload) state.pdfs.splice(index, 1);
+      });
       return state;
     },
-    clearUser: (state,action) => {
+    clearUser: (state, action) => {
       state = initState;
       return state;
-    },                                                                         //reset the store back to its default
+    }, //reset the store back to its default
   },
 });
 
-export const {actions, reducer} = userSlice;
+export const { actions, reducer } = userSlice;
 
-export const {setUser,setEmail,setColour,addPDF,removePDF,clearUser} = actions;
+export const { setUser, setEmail, setColour, addPDF, removePDF, clearUser } =
+  actions;
 
 //export default reducer
 
