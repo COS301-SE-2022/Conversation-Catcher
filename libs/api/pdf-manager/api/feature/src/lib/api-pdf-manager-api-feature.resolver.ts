90a5a4244e99cb4d5998ea5a2a3152b0e15528f9
Xander <u20465026@tuks.co.za>
2022-10-27 13:24:52 +0200

    ♻️: Update dates

--- libs/api/pdf-manager/api/feature/src/lib/api-pdf-manager-api-feature.resolver.ts
+++ libs/api/pdf-manager/api/feature/src/lib/api-pdf-manager-api-feature.resolver.ts
@@ -30,7 +30,7 @@ export class ApiPdfManagerApiFeatureResolver {
     const pdfObj = new PdfEntity();
     pdfObj.id = result._id;
     pdfObj.name = result.name;
-    pdfObj.creationDate = date.toUTCString();
+    pdfObj.creationDate = date.getHours() + ":" + date.getMinutes() + " " + date.toLocaleDateString();
     if (result.downloaded != null) pdfObj.downloaded = result.downloaded;
     else pdfObj.downloaded = false;
     pdfObj.text = result.text;
